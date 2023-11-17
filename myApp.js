7421f32c718cf6f896a913376f220379833d39f7
Ramón <ramon.mosquera@gmail.com>
2023-11-17 10:40:01 +0100

    3.- Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello world");
+// console.log("Hello world");
 
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+  res.send("/", `${__dirname}/views/index.html`);
 });
 
 module.exports = app;
