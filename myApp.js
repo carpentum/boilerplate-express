01a50590963c0fa8e7b774255e3dc8d2fcbb3ddd
Ramón <ramon.mosquera@gmail.com>
2023-11-22 11:33:22 +0100

    6.- Use the .env File

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", jsonHandler);
 function jsonHandler(req, res) {
   let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === "uppercase") {
+  if (process.env["MESSAGE_STYLE"] === "uppercase") {
     message = message.toUpperCase();
   }
   res.json({
